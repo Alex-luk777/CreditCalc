23dd2449564425aec5cf9126d6ca7e149a5fbb8c
Alex <lilipyt@gmail.com>
2021-08-04 16:09:39 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.text.Format;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter period in month:");
+        Scanner scanner= new Scanner(System.in);
+        byte month= scanner.nextByte();
+        System.out.print("Enter Total price: ");
+        int price=scanner.nextInt();
+        System.out.print("Enter persentage per month: ");
+
+        float percent=scanner.nextFloat();
+        float result =  price*month*percent/100;
+        float result2= result/price;
+        System.out.println("You will pay: "+ (result+price));
+        NumberFormat percentValue=NumberFormat.getPercentInstance();
+        String resultPercent= percentValue.format(result2);
+        System.out.print("Extra pay is "+ resultPercent);
+
+    }
+
+}
